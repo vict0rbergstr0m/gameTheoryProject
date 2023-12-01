0b9755704709b41227dfea581b09a2af284200d3
vict0rbergstr0m <89547216+vict0rbergstr0m@users.noreply.github.com>
2023-12-01 14:54:15 +0100

    it works, just need to make it interesting

--- code/main.py
+++ code/main.py
@@ -39,16 +39,16 @@ for i in range(n_rounds):
 
     print("\nnash equilibria:");
     equilibria = game.support_enumeration();
-    eq_sum = np.array([[1/10,1/10,1/10],[1/10,1/10,1/10]])
+    eq_sum = np.array([[1/10,1/10,1/10],[1/10,1/10,1/10]]) #default probabilities, 0 will give error....
     for eq in equilibria:
         print(eq);
-        eq_sum[0] = eq_sum[0] + eq[0];
+        eq_sum[0] = eq_sum[0] + eq[0]; #sum all nash equilibria
         eq_sum[1] = eq_sum[1] + eq[1];
     eq_sum[0] = eq_sum[0]/(eq_sum[0][0]+eq_sum[0][1]+eq_sum[0][2]);
     eq_sum[1] = eq_sum[1]/(eq_sum[1][0]+eq_sum[1][1]+eq_sum[1][2]);
 
     available_actions = [0, 1, 2]
-    actions = [np.random.choice(available_actions, p=eq_sum[0]), 
+    actions = [np.random.choice(available_actions, p=eq_sum[0]),  #choose action based on probabilities of sum of nash equilibria
                 np.random.choice(available_actions, p=eq_sum[1])]; # 0 = harvest, 1 = raid, 2 = trade
     
     
